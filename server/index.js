5af6f73d8d81f4933defc4457fbf119f8d7d4a6e
clive <70014577+clive-cudi@users.noreply.github.com>
2022-05-04 21:22:44 +0300

    env_config

--- server/index.js
+++ server/index.js
@@ -1,38 +1,10 @@
 import { ApolloError, ApolloServer, gql} from 'apollo-server';
 import mongoose from 'mongoose';
-const URI = `mongodb+srv://admin:cloud1234@cluster0.gbgg4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const URI = process.env.MONGO_URI;
 import { typeDefs } from './schemas/index.js';
 import { resolvers } from './resolvers/index.js';
 
 mongoose.connect(URI, {useNewUrlParser: true});
-
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-];
-
-// const typeDefs = gql`
-//     type Book {
-//         title: String
-//         author: String
-//     }
-
-//     type Query {
-//         books: [Book]
-//     }
-// `;
-
-// const resolvers = {
-//     Query: {
-//         books: ()=> books,
-//     },
-// };
 
 const connection = mongoose.connection;
 
